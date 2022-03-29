81576f2ad6a5fec66c6254aab36f7dc0c5987f97
jameshamling <93711665+jameshamling@users.noreply.github.com>
2022-03-29 16:57:58 +0100

    add media queries for large screens
    - media queries for 700px & 900px
    - in JS add ability to close menu by clicking outside of it

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const menuBtn = document.getElementById("menu-icon")
 const menu = document.getElementById("header-menu")
 
+// menu is opened or closed when button is clicked
 
 menuBtn.addEventListener("click", () => {
     menu.style.display = menu.style.display === "block" ?
     "none" :
     "block"
+})
+
+// menu disappears when user clicks away from it
+
+document.addEventListener("click", event => {
+    if ( !menu.contains(event.target) && !menuBtn.contains(event.target)) {
+        menu.style.display = "none"
+    }
 })
